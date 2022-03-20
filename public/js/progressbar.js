07b16b8788d0eb4a6da0899c8d0f58781eb0486d
tor2dbear <99970714+tor2dbear@users.noreply.github.com>
2022-03-20 18:16:33 +0100

    Delete public directory

--- public/js/progressbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {progressBar()};
-
-function progressBar() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100 + "%";
-  var corr = (winScroll / height) * 160 + "px";
-  document.getElementById("myBar").style.width = "calc(" + scrolled + " - " + corr + ")";
-}
