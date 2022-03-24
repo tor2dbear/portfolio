744630b001a2308642986770966d7ca4e39b66f2
tor2dbear <tor2dbear@users.noreply.github.com>
2022-03-24 11:17:22 +0000

    deploy: 16f0ca5b7dddd52738decb11438a501ea4b359c9

--- js/progressbar.js
+++ js/progressbar.js
@@ -3,7 +3,9 @@ window.onscroll = function() {progressBar()};
 function progressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100 + "%";
-  var corr = (winScroll / height) * 160 + "px";
-  document.getElementById("myBar").style.width = "calc(" + scrolled + " - " + corr + ")";
+  var scrolledleft = (winScroll / height) * 30 + "%";
+  var scrolledright = (winScroll / height) * 70 + "%";
+  var corr = (winScroll / height) * 80 + "px";
+  document.getElementById("hyphen-left").style.width = "calc(" + scrolledleft + " - " + corr + ")";
+  document.getElementById("hyphen-right").style.width = "calc(" + scrolledright + " - " + corr + ")";
 }
