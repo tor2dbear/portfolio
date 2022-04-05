257f51e5baa3e2fa805a1388aa3f91a1ecce69fa
Torbjörn <tb.hedberg@gmail.com>
2022-04-05 22:11:12 +0200

    r

--- static/js/progressbar.js
+++ static/js/progressbar.js
@@ -1,13 +1,11 @@
 window.onscroll = function() {progressBar()};
 
-
 function progressBar() {
-  var test = getComputedStyle(document.documentElement).getPropertyValue('--progressbarcorrection');
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolledleft = (winScroll / height) * 30 + "%";
   var scrolledright = (winScroll / height) * 70 + "%";
-  var corr = (winScroll / height) * test + "rem";
+  var corr = (winScroll / height) * 5.4 + "rem";
   document.getElementById("hyphen-left").style.width = "calc(" + scrolledleft + " - " + corr + ")";
   document.getElementById("hyphen-right").style.width = "calc(" + scrolledright + " - " + corr + ")";
 }
