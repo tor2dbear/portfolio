df0227ceb686e9e08a8810310060b9f09b0015db
tor2dbear <tor2dbear@users.noreply.github.com>
2022-05-29 17:38:33 +0000

    deploy: b79a2f8e4ad06d6deed6dff73102b2cfcc39e5ef

--- js/header-hide.js
+++ js/header-hide.js
@@ -1,14 +1,80 @@
-  let prevScrollpos = window.scrollY
-    const headerhide = document.querySelector("#topmenu")
+    (function(){
 
-    const add_class_on_scrol = () => headerhide.classList.add("hide")
-    const remove_class_on_scrol = () => headerhide.classList.remove("hide")
-
-    window.addEventListener('scroll', function() { 
-      currentScrollPos = window.scrollY;
-
-      if (prevScrollpos > currentScrollPos) { remove_class_on_scrol() }
-      else { add_class_on_scrol() }
-
-      prevScrollpos = currentScrollPos;
-    })
+      var doc = document.documentElement;
+      var w   = window;
+  
+      /*
+      define four variables: curScroll, prevScroll, curDirection, prevDirection
+      */
+  
+      var curScroll;
+      var prevScroll = w.scrollY || doc.scrollTop;
+      var curDirection = 0;
+      var prevDirection = 0;
+    
+      var lastY = 0;
+  
+      /*
+      how it works:
+      -------------
+      create a scroll event listener
+      create function to check scroll position on each scroll event,
+      compare curScroll and prevScroll values to find the scroll direction
+      scroll up - 1, scroll down - 2, initial - 0
+      then set the direction value to curDirection
+      compare curDirection and prevDirection
+      if it is different, call a function to show or hide the header
+      example:
+      step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
+      step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
+      step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
+      */
+  
+      var header = document.getElementById('topmenu');
+      var toggled;
+      var downThreshold = 300;
+      var upThreshold = 200;
+  
+      var checkScroll = function() {
+          curScroll = w.scrollY || doc.scrollTop;
+          if(curScroll > prevScroll) {
+              // scrolled down
+              curDirection = 2;
+          }
+          else {
+              //scrolled up
+              curDirection = 1;
+          }
+  
+          if(curDirection !== prevDirection) {
+              toggled = toggleHeader();
+          } else {
+              lastY=curScroll
+          }
+  
+          prevScroll = curScroll;
+          if(toggled) {
+              prevDirection = curDirection;
+          }
+      };
+  
+      var toggleHeader = function() { 
+          toggled = true;
+          if(curDirection === 2 && (curScroll-lastY) > downThreshold) {
+              lastY=curScroll
+              header.classList.add('hide');
+          }
+          else if (curDirection === 1 && (lastY-curScroll) > upThreshold) {
+              lastY=curScroll
+              header.classList.remove('hide');
+          }
+          else {
+              toggled = false;
+          }
+          return toggled;
+      };
+  
+      window.addEventListener('scroll', checkScroll);
+  
+  })();
+    
