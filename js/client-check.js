23c06d6d316df12090fa6df3ec4fa630a7ad20d8
tor2dbear <tor2dbear@users.noreply.github.com>
2022-06-15 19:26:40 +0000

    deploy: 3bd0a48a8fd48f60b7185014f4e13bd1fa25c17e

--- js/client-check.js
+++ js/client-check.js
@@ -1,8 +1,10 @@
-var layoutClient = document.getElementById('layout');
+var layoutClient = document.getElementById("layout");
 const queryString = window.location.search;
 const clientsDir = window.location.href.indexOf("tutorial") > -1;
 
-if (queryString.includes("?source=client") || (window.location.href.indexOf("/clients/") > -1)) {
-    layoutClient.classList.add('clientpage');
+if (
+  queryString.includes("?source=client") ||
+  window.location.href.indexOf("/clients/") > -1
+) {
+  layoutClient.classList.add("clientpage");
 }
-
