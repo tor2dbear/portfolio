78e6dae84f565cdd7fd8d5308ef60703a2db0a56
tor2dbear <tor2dbear@users.noreply.github.com>
2022-06-17 11:48:44 +0000

    deploy: 3b61852d321aa6df732a5476e7c3dffa5e3c834e

--- js/client-set-cookie.js
+++ js/client-set-cookie.js
@@ -1,4 +1,5 @@
 const clientUrl = window.location.href;
-var cookie = "clientUrl=" + clientUrl + "; path=/";
-document.cookie = cookie;
-let cookies = document.cookie;
+const clientName = document.getElementsByClassName("company_name")[0].innerHTML;
+
+localStorage.setItem("lsClientUrl", clientUrl);
+localStorage.setItem("lsClient", clientName);
