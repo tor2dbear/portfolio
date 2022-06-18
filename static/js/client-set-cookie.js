317fe75212b579b506a870277ad6a5ac7c4cde7b
Torbjörn <tb.hedberg@gmail.com>
2022-06-18 12:48:30 +0200

    removed potential anchor link from localStorage

--- static/js/client-set-cookie.js
+++ static/js/client-set-cookie.js
@@ -1,5 +1,7 @@
-const clientUrl = window.location.href;
+const clientUrl = window.location.href.match(/(^[^#]*)/)[0];
 const clientName = document.getElementsByClassName("company_name")[0].innerHTML;
 
 localStorage.setItem("lsClientUrl", clientUrl);
 localStorage.setItem("lsClient", clientName);
+
+console.log(clientUrl);
