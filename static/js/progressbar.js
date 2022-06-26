6871e1b50065d82f236e813a243e67c3db5410dc
tor2dbear <tb.hedberg@gmail.com>
2022-06-26 13:44:19 +0200

    html 5 elements and brand href js div

--- static/js/progressbar.js
+++ static/js/progressbar.js
@@ -1,11 +1,35 @@
-window.onscroll = function() {progressBar()};
+window.onscroll = function () {
+  progressBar();
+};
+
+let first = document.querySelector(".brand-first");
+let widthFirst = first.offsetWidth;
+let second = document.querySelector(".brand-second");
+let widthSecond = second.offsetWidth;
+let third = document.querySelector(".brand-third");
+let widthThird = third.offsetWidth;
+let fourth = document.querySelector(".brand-fourth");
+let widthFourth = fourth.offsetWidth;
+let fifth = document.querySelector(".brand-fifth");
+let widthFifth = fifth.offsetWidth;
+let brandBase =
+  widthFirst + widthSecond + widthThird + widthFourth + widthFifth;
+let brandWidth = brandBase + 5 + "px";
 
 function progressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolledleft = (winScroll / height) * 30 + "%";
   var scrolledright = (winScroll / height) * 70 + "%";
   var corr = (winScroll / height) * 5.4 + "rem";
-  document.getElementById("hyphen-left").style.width = "calc(" + scrolledleft + " - " + corr + ")";
-  document.getElementById("hyphen-right").style.width = "calc(" + scrolledright + " - " + corr + ")";
+  var scrolledBrand = (winScroll / height) * 100 + "%";
+  var corrBrand = 1 - winScroll / height;
+  document.getElementById("hyphen-left").style.width =
+    "calc(" + scrolledleft + " - " + corr + ")";
+  document.getElementById("hyphen-right").style.width =
+    "calc(" + scrolledright + " - " + corr + ")";
+  document.getElementById("brand-link").style.width =
+    "calc(" + brandWidth + " * " + corrBrand + " + " + scrolledBrand + ")";
 }
