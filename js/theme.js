7d19a7b38613575eda2ca98343353efb24c13eb6
tor2dbear <tor2dbear@users.noreply.github.com>
2024-06-25 07:50:56 +0000

    deploy: 94f29555dba6322e2a5c7cd8289f843d596251c7

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,40 @@
 var toggle = document.getElementById("theme-toggle");
 var delay = document.body;
 
-var storedTheme = localStorage.getItem('mode') || (window.matchMedia("(prefers-color-scheme: pant)").matches ? "pant" : "standard");
+var storedTheme =
+  localStorage.getItem("mode") ||
+  (window.matchMedia("(prefers-color-scheme: pant)").matches
+    ? "pant"
+    : "standard");
 if (storedTheme)
-    document.documentElement.setAttribute('data-mode', storedTheme)
+  document.documentElement.setAttribute("data-mode", storedTheme);
 
-toggle.onclick = function() {
-    var currentMode = document.documentElement.getAttribute("data-mode");
-    var targetMode = "standard";
-    delay.classList.add("darkmodeTransition");
-    setTimeout(function() {
-        delay.classList.remove("darkmodeTransition");
-      }, 1000);
+toggle.onclick = function () {
+  var currentMode = document.documentElement.getAttribute("data-mode");
+  var targetMode = "standard";
+  delay.classList.add("darkmodeTransition");
+  setTimeout(function () {
+    delay.classList.remove("darkmodeTransition");
+  }, 1000);
 
-    if (currentMode === "standard") {
-        targetMode = "pant";
-    }
+  if (currentMode === "standard") {
+    targetMode = "pant";
+  }
 
-    document.documentElement.setAttribute('data-mode', targetMode)
-    localStorage.setItem('mode', targetMode);
+  document.documentElement.setAttribute("data-mode", targetMode);
+  localStorage.setItem("mode", targetMode);
 };
+// Set the meta tag content based on the CSS variable
+// Function to get the value of a CSS variable
+function getCssVariableValue(variableName) {
+  return getComputedStyle(document.documentElement).getPropertyValue(
+    variableName
+  );
+}
+
+// Set the theme color meta tag content to the value of --bg-color
+const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+themeColorMetaTag.setAttribute(
+  "content",
+  getCssVariableValue("--bg-color").trim()
+);
