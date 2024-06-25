7b197fd2e2abd56b8d863bae499c70a0d7ec659d
tor2dbear <tb.hedberg@gmail.com>
2024-06-25 09:45:37 +0200

    icon changes and meta theme color

--- static/js/recaptcha.js
+++ static/js/recaptcha.js
@@ -1,7 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function onSubmit(token) {
+    document.getElementById("recaptchaToken").value = token;
+    var form = document.getElementById("contact-form");
+
+    // Handle form submission via JavaScript
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", form.action, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onload = function () {
+      var successMessage = document.getElementById("successMessage");
+      var errorMessage = document.getElementById("errorMessage");
+      if (xhr.status >= 200 && xhr.status < 400) {
+        successMessage.style.display = "block";
+        errorMessage.style.display = "none";
+        form.reset();
+      } else {
+        errorMessage.style.display = "block";
+        successMessage.style.display = "none";
+      }
+    };
+
+    xhr.onerror = function () {
+      var errorMessage = document.getElementById("errorMessage");
+      errorMessage.style.display = "block";
+      var successMessage = document.getElementById("successMessage");
+      successMessage.style.display = "none";
+    };
+
+    xhr.send(new URLSearchParams(formData).toString());
+  }
+
   grecaptcha.ready(function () {
     document
-      .querySelector(".g-recaptcha")
+      .getElementById("submitButton")
       .addEventListener("click", function (event) {
         event.preventDefault();
         grecaptcha
@@ -9,60 +42,13 @@ document.addEventListener("DOMContentLoaded", function () {
             action: "submit",
           })
           .then(function (token) {
-            document.getElementById("recaptchaToken").value = token;
-            var form = document.getElementById("contact-form");
-
-            // Prevent default form submission
-            form.addEventListener("submit", function (event) {
-              event.preventDefault();
-            });
-
-            // Handle form submission via JavaScript
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", form.action, true);
-            xhr.setRequestHeader(
-              "Content-Type",
-              "application/x-www-form-urlencoded"
-            );
-
-            // Disable the submit button
-            var submitButton = document.getElementById("submitButton");
-            submitButton.disabled = true;
-
-            xhr.onload = function () {
-              var successMessage = document.getElementById("successMessage");
-              var errorMessage = document.getElementById("errorMessage");
-              if (xhr.status >= 200 && xhr.status < 400) {
-                // Successful form submission
-                successMessage.style.display = "block";
-                errorMessage.style.display = "none";
-              } else {
-                // Handle error
-                errorMessage.style.display = "block";
-                successMessage.style.display = "none";
-                // Re-enable the submit button in case of error
-                submitButton.disabled = false;
-              }
-            };
-
-            xhr.onerror = function () {
-              var errorMessage = document.getElementById("errorMessage");
-              errorMessage.style.display = "block";
-              var successMessage = document.getElementById("successMessage");
-              successMessage.style.display = "none";
-              // Re-enable the submit button in case of error
-              submitButton.disabled = false;
-            };
-
-            xhr.send(new URLSearchParams(new FormData(form)).toString());
+            onSubmit(token);
           });
       });
 
-    // Prevent form's default submission
-    document
-      .getElementById("contact-form")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-      });
+    var form = document.getElementById("contact-form");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+    });
   });
 });
